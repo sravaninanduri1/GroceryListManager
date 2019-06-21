967d1f5da607aaf66a5ba873bb524c6c30675f84
sravani <sravaninanduri1@gmail.com>
2019-06-20 18:14:26 -0700

    added edit,delete and strike

--- src/app/app-grocery/app-grocery.component.ts
+++ src/app/app-grocery/app-grocery.component.ts
@@ -12,12 +12,48 @@ import { Component, OnInit } from '@angular/core';
   //ngOnInit() {
   //}
   export class AppGroceryComponent {
-    task: string;
+    task = {
+      name: '',
+      id: 0
+    };
     tasks = [];
-   
+
     onClick(){
-      this.tasks.push({name: this.task});
-      this.task = '';
+      if(this.task.id == 0){
+        this.tasks.push({id: (new Date()).getTime(),name: this.task.name, strike: false});
+      }
+      
+      this.task = {
+        name: '',
+        id: 0
+      };
+    }
+    
+    onEdit(item){
+      this.task = item;
+    }
+
+    onDelete(item){
+      for(var i = 0;i < this.tasks.length; i++){
+        if(item.id == this.tasks[i].id){
+          this.tasks.splice(i,1);
+          break;
+        }
+      }
+    }
+
+    onStrike(item){
+      for(var i = 0;i < this.tasks.length; i++){
+        if(item.id == this.tasks[i].id){
+          if(this.tasks[i].strike){
+            this.tasks[i].strike = false;
+          }
+          else{
+            this.tasks[i].strike = true;
+          }
+          break;
+        }
+      }
     }
 
 }
